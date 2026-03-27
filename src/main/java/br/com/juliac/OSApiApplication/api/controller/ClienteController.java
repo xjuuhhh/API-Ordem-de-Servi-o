df6b3efe3cf96ce136929a76daeca8a352f8ec51
xjuuhhh <juliasantoscursos05@gmail.com>
2026-03-27 16:52:10 -0300

    Finalização das anotações Swagger ClienteController

--- src/main/java/br/com/juliac/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/com/juliac/OSApiApplication/api/controller/ClienteController.java
@@ -90,15 +90,43 @@ public class ClienteController {
      * @param cliente
      * @return 
      */
+    @Operation(summary = "Adiciona um novo cliente", description = "Cria um novo registro de cliente")
+    @ApiResponses(value ={
+        @ApiResponse(responseCode = "201", description = "Cliente criado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados do cliente inválidos")
+    })
     @PostMapping("/clientes")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+    public Cliente adicionar(
+            @Parameter(description = "Representação de um novo cliente", required = true)
+            @Valid 
+            @RequestBody 
+            Cliente cliente) {
         return clienteService.salvar(cliente);
     }
     
+    /**
+     * 
+     * @param clienteID
+     * @param cliente
+     * @return 
+     */
+    @Operation(summary = "Atualiza um cliente", description = "Atualiza os dados de um cliente ja existente")
+    @ApiResponses(value ={
+        @ApiResponse(responseCode = "200", description = "Cliente atualzado com sucesso"),  
+        @ApiResponse(responseCode = "400", description = "Dados do cliente inválidos"),
+        @ApiResponse(responseCode = "404", description = "Cliente não encontrado")
+    })
     @PutMapping("/clientes/{clienteID}")
-    public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteID,
-                                            @RequestBody Cliente cliente) {
+    public ResponseEntity<Cliente> atualizar(
+            //Etiqueta para o id que vai na URL
+            @Parameter(name = "id", description = "ID do cliente", example= "1")
+            @PathVariable Long clienteID,
+            
+            //Etiqueta para os dados que vai atualizar
+            @Parameter(description = "Dados atualizados do cliente", required = true)
+            @Valid @RequestBody Cliente cliente
+    ) {
         //Verifica se o cliente existe
         if (!clienteRepository.existsById(clienteID)) {
             return ResponseEntity.notFound().build();
@@ -109,8 +137,21 @@ public class ClienteController {
         return ResponseEntity.ok(cliente);
     }
     
+    
+    /**
+     * 
+     * @param clienteID
+     * @return 
+     */
+    @Operation(summary="Deleta cliente", description="Deleta um cliente e seus dados")
+    @ApiResponses(value ={
+        @ApiResponse(responseCode = "204", description="Cliente deletado com sucesso"),
+        @ApiResponse(responseCode = "404", description="Cliente não encontrado")
+    })
     @DeleteMapping("/clientes/{clienteID}")
-    public ResponseEntity<Void> excluir(@PathVariable Long clienteID) {
+    public ResponseEntity<Void> excluir(
+            @Parameter(name="clienteID", description = "ID do cliente para exclusão", example = "1")
+            @PathVariable Long clienteID) {
         //Verifica se cliente existe ou não
         
         if (!clienteRepository.existsById(clienteID)){
